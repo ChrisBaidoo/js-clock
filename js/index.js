72c566eb635b7f602298ee6ad7eb759126f61fef
chrisbaidoo <chris.baidoo@smartpension.co.uk>
2019-04-02 06:01:43 +0100

    Added leading zeros

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
 function renderTime() {
   const date = new Date();
-  const hour = date.getHours();
-  const min = date.getMinutes();
-  const sec = date.getSeconds();
+  const hour = addZero(date.getHours());
+  const min = addZero(date.getMinutes());
+  const sec = addZero(date.getSeconds());
 
   document.getElementById('clock').innerText = hour + ':' + min + ':' + sec;
+
+}
+
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
 
 renderTime();
+window.setInterval(renderTime, 1000);
 
-window.setInterval(function() {
-  renderTime();
-}, 1000);
+// function display() {
+//   console.log("Hard Work Beats Talent When Talent Doesnt Work Hard")
+// }
+// 
+// var button = document.getElementById("click")
+// button.addEventListener('click', display, false)
